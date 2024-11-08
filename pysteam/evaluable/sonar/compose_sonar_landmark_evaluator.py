a44b582d192725aabf851c5ac084d0cc3636e45c
itskalvik <itskalvik@gmail.com>
2024-11-08 16:18:45 -0600

    Fix sonar sensor model forward pass

--- pysteam/evaluable/sonar/compose_sonar_landmark_evaluator.py
+++ pysteam/evaluable/sonar/compose_sonar_landmark_evaluator.py
@@ -20,14 +20,14 @@ def angle_between(v1, v2):
     """
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))[0]
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 class ComposeSonarLandmarkEvaluator(Evaluable):
     """Evaluator for the composition of a transformation evaluator and sonar landmark state."""
 
     def __init__(self, transform: Evaluable, landmark: Evaluable):
         super().__init__()
-        self._transform: Evaluable = transform # Sonar Pose (coincident with robot pose)
+        self._transform: Evaluable = transform # Sonar Pose (coincident with robot pose, T_wr)
         self._landmark: Evaluable = landmark # landmark x, y, z in world frame (homogeneous coordinates)
 
     @property
@@ -43,24 +43,30 @@ class ComposeSonarLandmarkEvaluator(Evaluable):
         landmark_child = self._landmark.forward()
 
         # Convert landmark from world frame to robot frame
-        landmark_robot = transform_child.value.matrix() @ landmark_child.value
+        landmark_robot = transform_child.value.inverse().matrix() @ landmark_child.value
+
+        print("Forward")
+        print("T_rw:\n", transform_child.value.inverse().matrix())
+        print("P_w:\n", landmark_child.value)
+        print("P_r:\n", landmark_robot)
 
         # Get angle between each axis and landmark
         if np.sum(landmark_robot[:3]) > 0:
-            distance = np.linalg.norm(landmark_robot[:3])
-            alpha = angle_between((1, 0, 0), landmark_robot[:3])
-            beta = angle_between((0, 1, 0), landmark_robot[:3])
-            gamma = angle_between((0, 0, 1), landmark_robot[:3])
-            print(distance, alpha, beta, gamma)
+            # Distance to point in robot frame
+            distance = np.linalg.norm(landmark_robot[:3, 0])
+            # Angle between x-axis and point (ideally the sonar angle)
+            theta = angle_between((1, 0, 0), landmark_robot[:3, 0])
+            # Angle between z-axis and point (ideally 1.5708, i.e., perpendicular to the robot's z-axis)
+            # Assuming that the robot's front direction is along the z-axis
+            gamma = angle_between((0, 0, 1), landmark_robot[:3, 0])
         else:
+            # Base case if the estimated landmark is at [0, 0, 0, 1]
             distance = 0.0
-            alpha = 0.0
-            beta = 0.0
+            theta = 0.0
             gamma = 0.0
 
         value = np.array([distance,
-                          alpha,
-                          beta,
+                          theta,
                           gamma]).reshape(-1, 1)        
 
         return Node(value, transform_child, landmark_child)
--- pysteam/evaluable/sonar/sonar_error_evaluator.py
+++ pysteam/evaluable/sonar/sonar_error_evaluator.py
@@ -8,15 +8,15 @@ class SonarErrorEvaluator(Evaluable):
 
     def __init__(self, pt: Evaluable, meas_pt: np.ndarray):
         super().__init__()
+        # compose_sonar_landmark_evaluator output 
+        # [distance, theta]
         self._pt: Evaluable = pt
-        self._meas_pt: np.ndarray = meas_pt
-        self._D = np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-            ]
-        )
+
+        # Sonar landmark in robot frame
+        # [distance, alpha, beta, gamma]
+        self._meas_pt: np.ndarray = np.zeros((3, 1))
+        self._meas_pt[:2] = meas_pt
+        self._meas_pt[2] = 1.570
 
     @property
     def active(self) -> bool:
@@ -27,10 +27,15 @@ class SonarErrorEvaluator(Evaluable):
         return self._pt.related_var_keys
 
     def forward(self) -> Node:
-        child = self._pt.forward()
-        value = self._D @ (self._meas_pt - child.value)
-        return Node(value, child)
+        landmark_est = self._pt.forward()
+        value = np.sum(self._meas_pt - landmark_est.value)
+
+        print("Landmark Est:\n", landmark_est.value)
+        print("Landmark GT:\n", self._meas_pt)
+        print("Error:", value)
+
+        return Node(value, landmark_est)
 
     def backward(self, lhs: np.ndarray, node: Node, jacs: Jacobians) -> None:
         if self._pt.active:
-            self._pt.backward(-lhs @ self._D, node.children[0], jacs)
+            self._pt.backward(-lhs, node.children[0], jacs)
